0b95f20ed79e74bab95c8390edc313006d80da87
Edward Knowles <mail@eknowles.com>
2018-08-20 17:19:54 +0100

    feat(Button): add 4 new button sizes

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -4,7 +4,26 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import Button from './Button';
+import Button, { Size } from './Button';
+
+storiesOf('Kitchen Sink', module)
+  .add('Buttons', () => {
+    const sizes = [Size.Small, Size.Medium, Size.Large, Size.Jumbo];
+    return (
+      <div>
+        {sizes.map((size: number) => (
+          <Button key={size} size={size} onClick={() => {}}>
+            <span>Button Size {size}</span>
+          </Button>
+        ))}
+      </div>
+    );
+  })
+;
 
 storiesOf('Components/Button', module)
-  .add('Default', () => <Button onClick={() => {}}>Default Button</Button>);
+  .add('Small', () => <Button size={Size.Small} onClick={() => {}}>Small Button</Button>)
+  .add('Medium', () => <Button size={Size.Medium} onClick={() => {}}>Medium Button</Button>)
+  .add('Large', () => <Button size={Size.Large} onClick={() => {}}>Large Button</Button>)
+  .add('Jumbo', () => <Button size={Size.Jumbo} onClick={() => {}}>Jumbo Button</Button>)
+;
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,23 +1,37 @@
 import { MouseEventHandler } from 'react';
 import * as React from 'react';
+import cx from 'classnames';
 //@ts-ignore
 import * as styles from './Button.pcss';
 
+export enum Size { Small, Medium, Large, Jumbo }
 export interface IButtonProps {
-  children: string;
+  children?: any;
+  size: Size;
   onClick(event: MouseEventHandler<HTMLButtonElement>): void;
 }
 
 export default class Button extends React.PureComponent<IButtonProps, {}> {
+  static defaultProps = {
+    size: Size.Medium,
+  };
+
+  static sizeClass = {
+    [Size.Small]: styles.sizeSmall,
+    [Size.Medium]: styles.sizeMedium,
+    [Size.Large]: styles.sizeLarge,
+    [Size.Jumbo]: styles.sizeJumbo,
+  };
+
   render() {
     return (
       <button
-        className={styles.button}
+        className={cx(styles.button, Button.sizeClass[this.props.size])}
         onClick={this.props.onClick.bind(this)}
         type="button"
       >
         {this.props.children}
       </button>
-    )
+    );
   }
 }
